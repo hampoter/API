1bc8cd6bd99f9028039ced7e77a4ef975cfba174
hampoter <122030111+hampoter@users.noreply.github.com>
2023-02-07 01:37:34 +0500

    Update find_token.php

--- lepain/find_token.php
+++ lepain/find_token.php
@@ -1,74 +1,74 @@
 <?php
 
-include_once("db_connect.php");
-
 if(!isset($_GET)){
     echo ajax_echo(
-        "Ошибка", 
-        "GET параметр отсутствует",
-        true,
-        "ERROR",
-        null
-
+        "Ошибка!", 
+        "Вы не указали GET параметры", 
+        true, 
+        "ERROR", 
+        null 
     );
     exit();
 }
 
 if(!isset($_GET['token'])){
     echo ajax_echo(
-        "Ошибка", 
-        "Отсутствует GET параметр token",
-        true,
-        "ERROR",
-        null
-
+        "Ошибка!", 
+        "Вы не указали GET параметр token",
+        true, 
+        "ERROR", 
+        null 
     );
     exit();
 }
 
+define('TOKEN', $_GET['token']);
 
-
-
-
-define("TOKEN", $_GET['token']);
-
-if(preg_match_all("/^[A-z0-9_]{32}/ui", TOKEN)){
+if(!preg_match_all("/^[A-z0-9_]{32}$/iu", TOKEN)){
     echo ajax_echo(
-        "Ошибка", 
-        "Токен не соответствует параметрам",
-        true,
-        "ERROR",
-        null
-
+        "Ошибка!", 
+        "Ваш токен не соответствует шаблону", 
+        true, 
+        "ERROR", 
+        null 
     );
     exit();
 }
 
-$query = "SELECT COUNT(`id`) > 0 AS `RESULT` FROM `API_clients` WHERE `token` = \"".TOKEN."\"";
+$query = "SELECT COUNT(`id`) > 0 AS 'RESULT' FROM `tokens` WHERE `token` = '" . TOKEN . "' AND `ban` = FALSE";
 $res_query = mysqli_query($connection, $query);
 
 if(!$res_query){
     echo ajax_echo(
-        "Ошибка", 
-        "Ошибка в запросе",
-        true,
-        "ERROR",
-        null
-
+        "Ошибка!", 
+        "Ошибка в запросе.", 
+        true, 
+        "ERROR", 
+        null 
     );
     exit();
 }
 
 $res = mysqli_fetch_assoc($res_query);
 
+if(!$res){
+    echo ajax_echo(
+        "Ошибка!", 
+        "Ошибка в запросе.", 
+        true, 
+        "ERROR", 
+        null 
+    );
+    exit();
+}
+
 if($res['RESULT'] == '0'){
     echo ajax_echo(
-        "Ошибка", 
-        "Токен не действителен",
-        true,
-        "ERROR",
-        null
-
+        "Ошибка!", 
+        "Ваш токен не является действительным!", 
+        true, 
+        "ERROR", 
+        null 
     );
     exit();
 }
