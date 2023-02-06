dbcbb8b16e37ab1f90d7c45671d22744270cd0a5
hampoter <122030111+hampoter@users.noreply.github.com>
2023-02-06 10:23:15 +0500

    Update index.php

--- lepain/index.php
+++ lepain/index.php
@@ -1,7 +1,7 @@
 <?php 
 
 include_once("config.php");
-include_once("err_handler.php");
+include_once("error_handler.php");
 include_once("db_connect.php");
 include_once("functions.php");
 include_once("find_token.php");
